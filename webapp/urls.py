6661b2162d42a8c1afc6bd7610de94b544714e3e
ramandeep2 <raman_84deep@yahoo.com>
2018-04-01 14:57:14 +0530

    Upgrading Files to run in Django 2

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^contact/', views.contact, name='contact'),
-    url(r'^postQuery/', views.postQuery, name='contact'),
-    url(r'^confirmation/', views.submitConfirmation, name='submitConfirmation')
+    url('^$', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('postQuery/', views.postQuery, name='contact'),
+    path('confirmation/', views.submitConfirmation, name='submitConfirmation')
     ]
